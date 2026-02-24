e995dfdde6142201daa5ff016d4307724daca580
ShruS02 <ss2896@srmist.edu.in>
2026-02-24 16:21:33 +0530

    usecase5

--- src/usecasepalindromeapp.java
+++ src/usecasepalindromeapp.java
@@ -1,28 +1,27 @@
+import java.util.Stack;
+
 public class usecasepalindromeapp {
 
     public static void main(String[] args) {
         // Hardcoded string to check
-        String word = "level";
+        String word = "deified";
 
-        // Convert string to character array
-        char[] chars = word.toCharArray();
+        // Create a stack
+        Stack<Character> stack = new Stack<>();
 
-        // Two-pointer approach
-        boolean isPalindrome = true;
-        int left = 0;
-        int right = chars.length - 1;
-
-        while (left < right) {
-            if (chars[left] != chars[right]) {
-                isPalindrome = false;
-                break;
-            }
-            left++;
-            right--;
+        // Push all characters into the stack
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
         }
 
-        // Display result
-        if (isPalindrome) {
+        // Pop characters and build reversed string
+        String reversed = "";
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
+        }
+
+        // Compare original and reversed strings
+        if (word.equals(reversed)) {
             System.out.println(word + " is a palindrome.");
         } else {
             System.out.println(word + " is not a palindrome.");
