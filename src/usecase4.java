3a857835a1e3c9d92416e4ed72072a8ff8371e09
ShruS02 <ss2896@srmist.edu.in>
2026-02-24 14:49:05 +0530

    adding usercase4

---
+++ src/usecase4.java
@@ -0,0 +1,31 @@
+public class usecase4 {
+
+    public static void main(String[] args) {
+        // Hardcoded string to check
+        String word = "level";
+
+        // Convert string to character array
+        char[] chars = word.toCharArray();
+
+        // Two-pointer approach
+        boolean isPalindrome = true;
+        int left = 0;
+        int right = chars.length - 1;
+
+        while (left < right) {
+            if (chars[left] != chars[right]) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
+    }
+}
