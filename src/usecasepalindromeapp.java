5b67f944a40b81caff3f2f35ae8796a60c4a2b67
ShruS02 <ss2896@srmist.edu.in>
2026-02-24 16:13:49 +0530

    usecase2

--- src/usecasepalindromeapp.java
+++ src/usecasepalindromeapp.java
@@ -1,10 +1,32 @@
-public class usecasepalindromeapp{
+public class usecasepalindromeapp {
 
     public static void main(String[] args) {
+        // Hardcoded string to check
+        String word = "madam";
 
-        System.out.println("Welcome to Palindrome Checker App");
-        System.out.println("Version 1.0");
-        System.out.println("Application started successfully.");
+        // Check if palindrome
+        boolean isPalindrome = isPalindrome(word);
 
+        // Print result
+        if (isPalindrome) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
+    }
+
+    // Helper method to check palindrome
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
